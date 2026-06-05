f98ad861abf35bce84e6b24a95bd759a6fddb0fc
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-06-05 20:22:01 +0200

    Add 'IA' command with placeholder message

--- terminal.py
+++ terminal.py
@@ -4,10 +4,12 @@ import time
 cmd = sys.argv[1] if len(sys.argv) > 1 else ""
 
 if cmd == "help":
-    print("Commandes : help, time, date")
+    print("Commandes : help, time, date, IA")
 elif cmd == "time":
     print(time.strftime("%H:%M:%S"))
 elif cmd == "date":
     print(time.strftime("%d/%m/%Y"))
+elif cmd == "IA":
+    print("À venir pour bientôt. Promis !")
 else:
     print(f"{cmd} est une commande inconnue.")
