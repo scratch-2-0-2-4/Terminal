bd80cd9ee1776deecfa9a22528f821b74559cb2f
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-06-06 21:23:52 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -5,7 +5,7 @@ import random
 cmd = sys.argv[1] if len(sys.argv) > 1 else ""
 
 if cmd == "help":
-    print("Commandes : help, time, date, IA, random, idée")
+    print("Commandes : help, time, date, IA, random, idée, ad")
 
 elif cmd == "time":
     print(time.strftime("%H:%M:%S"))
@@ -21,6 +21,9 @@ elif cmd == "random":
 
 elif cmd == "idée":
     print("Oh, tu as une idée de commande... Donne la à [@Scratch_2_0_2_4](https://scratch.mit.edu/users/Scratch_2_0_2_4/#comments)")
+
+elif cmd == "pub" or cmd == "ad" :
+    print("Contactez [@Scratch_2_0_2_4](https://scratch.mit.edu/users/Scratch_2_0_2_4/#comments) pour mettre votre pub ici.")
 
 else:
     print(f"{cmd} est une commande inconnue.")
