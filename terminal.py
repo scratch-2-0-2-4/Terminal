1ed43c9a0b0a2d0b15ba65c02a63721e61c7b26d
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-06-06 09:57:06 +0200

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,15 +1,23 @@
 import sys
 import time
+import random
 
 cmd = sys.argv[1] if len(sys.argv) > 1 else ""
 
 if cmd == "help":
-    print("Commandes : help, time, date, IA")
+    print("Commandes : help, time, date, IA, random")
+
 elif cmd == "time":
     print(time.strftime("%H:%M:%S"))
+
 elif cmd == "date":
     print(time.strftime("%d/%m/%Y"))
+
 elif cmd == "IA":
     print("À venir pour bientôt. Promis !")
+
+elif cmd == "random":
+    print(random.randint(1, 100))
+
 else:
     print(f"{cmd} est une commande inconnue.")
